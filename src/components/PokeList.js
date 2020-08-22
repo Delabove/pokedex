24cf2ec7564dade45a32a476f8875f743f4c33c2
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-22 18:30:47 -0500

    adding images for cells

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import './styles/PokeList.css';
 import PokeCell from './PokeCell';
+import { pokeClasses } from '../pokeClasses';
+import './styles/PokeList.css';
+
 
 const PokeList = () => {
+  const cells = pokeClasses.map(pokeClass => {
     return (
-        <section className="poke-list">
-        <PokeCell />
-        <PokeCell />
-        <PokeCell />
-        </section>
-    )
-    }
+      <PokeCell
+        key={pokeClass.id}
+        pokeClass={pokeClass}
+      />
+    );
+  });
 
-    export default PokeList;
+  return (
+    <section className="poke-list">
+      {cells}
+    </section>
+  )
+}
+export default PokeList;
+    //Instead of manually placing each PokeCell
+    //inside the parent element,  place
+    //the cells array, and React will append each of
+    // the PokeCells to the list component.
