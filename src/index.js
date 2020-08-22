f3309670027aa3765aa1cd7c6997d88bde58c264
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-22 15:22:25 -0500

    app constructor and css for the 2 views of app

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
 
 //This file basically is telling the DOM to render the App
 //component in the element with id of “root”. Our App component
