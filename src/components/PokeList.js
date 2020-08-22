7d2afaee7a3ce902fed2d4fa8850ffe9906e6e37
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-22 16:10:15 -0500

    added sprites

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './styles/PokeList.css';
+import PokeCell from './PokeCell';
 
 const PokeList = () => {
     return (
         <section className="poke-list">
-
+        <PokeCell />
+        <PokeCell />
+        <PokeCell />
         </section>
     )
     }
