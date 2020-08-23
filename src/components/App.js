f1b6362b06aeda2934bfcda0ea663cb7b65e7587
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-22 20:12:40 -0500

    fetch to get data from api

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './styles/App.css';
 import PokeList from './PokeList';
 import DetailView from './DetailView';
+import Pokeman from '.Pokemon';
 
 class App extends Component {
 
@@ -10,16 +11,32 @@ class App extends Component {
   // 1. constructor: where we initialize our state
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      pokemon: {}
+    };
+
+    this.handleOnClick = this.handleOnClick.bind(this);
   }
+
+  handleOnClick(id) {
+    fetch(`http://pokeapi.co/api/v2/pokemon/${id}/`)
+      .then(res => res.json())
+      .then(data => {
+        const pokemon = new Pokemon(data);
+
+        this.setState({ pokemon });
+      })
+      .catch(err => console.log(err));
+  }
+
 
   // 2. render: where we write our jsx
 
   render() {
     return (
       <div className="App">
-        <PokeList />
-        <DetailView />
+        <PokeList handleOnClick={this.handleOnClick} />
+        <DetailView pokemon={this.state.pokemon} />
       </div>
     );
   }
